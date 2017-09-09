97e04f848659f34f5b4be0eb7a3f84c3cbe7df1e
Will Khaine <wkhaine97@gmail.com>
2017-09-08 23:09:31 -0700

    Setup for Google Maps React integration, geolocation requests

--- src/pages/PlacePage.js
+++ src/pages/PlacePage.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react'
 import TextInput from '../components/TextInput'
 
 class PlacePage extends Component {
+  constructor() {
+    super()
+    this.params = {
+      "key" : "AIzaSyBuIYgWelclGIon27DfSCTrzcWLDubolCQ",
+      "location" : "",
+      "radius" : ""
+    }
+
+    this.geolocationAllowed = false
+    this.getGeolocation()
+  }
+
+  getGeolocation() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(this.getPlace);
+          this.geolocationAllowed = true 
+      } else {
+          console.log("Geolocation is not supported by this browser.");
+      }
+  }
+
   render() {
     return (
       <TextInput
